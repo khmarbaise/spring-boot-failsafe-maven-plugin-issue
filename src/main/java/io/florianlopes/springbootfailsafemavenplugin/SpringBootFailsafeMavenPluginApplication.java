c2c5583aa4ea089b58b47f3b5b78228b09abc001
Florian Lopes <florian.lopes@outlook.com>
2020-07-02 13:44:18 +0200

    Add projet files

---
+++ src/main/java/io/florianlopes/springbootfailsafemavenplugin/SpringBootFailsafeMavenPluginApplication.java
@@ -0,0 +1,13 @@
+package io.florianlopes.springbootfailsafemavenplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFailsafeMavenPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFailsafeMavenPluginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/florianlopes/springbootfailsafemavenplugin/SpringBootFailsafeMavenPluginApplicationIT.java
@@ -0,0 +1,13 @@
+package io.florianlopes.springbootfailsafemavenplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFailsafeMavenPluginApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
