216ce5bd13d376392e399342c53cc371f626e622
Karl Heinz Marbaise <kama@soebes.de>
2020-07-04 15:19:44 +0200

    Get Spring Boot IT's + JUnit Jupiter 5.7.0-M1 working

--- src/test/java/io/florianlopes/springbootfailsafemavenplugin/SpringBootFailsafeMavenPluginApplicationIT.java
+++ src/test/java/io/florianlopes/springbootfailsafemavenplugin/SpringBootFailsafeMavenPluginApplicationIT.java
@@ -3,7 +3,7 @@ package io.florianlopes.springbootfailsafemavenplugin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringBootFailsafeMavenPluginApplication.class)
 class SpringBootFailsafeMavenPluginApplicationIT {
 
     @Test
